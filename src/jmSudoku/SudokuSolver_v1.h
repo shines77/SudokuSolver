cba31325016cad5c2bceccea6ec901d4a30a5f57
shines77 <gz_shines@msn.com>
2020-12-05 06:31:00 +0800

    optimize: 优化: 移除了初始无用的 columns, 用查表法代替坐标乘除运算;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/jmSudoku/SudokuSolver_v1.h
+++ src/jmSudoku/SudokuSolver_v1.h
@@ -14,6 +14,7 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <cstring>      // For std::memset()
 #include <vector>
 #include <bitset>
 
@@ -42,12 +43,12 @@ public:
 
     static const size_t kCapacity = (Capacity + 1) / 2 * 2;
 
-    int prev[kCapacity];
-    int next[kCapacity];
-    int up[kCapacity];
-    int down[kCapacity];
-    int row[kCapacity];
-    int col[kCapacity];
+    unsigned short prev[kCapacity];
+    unsigned short next[kCapacity];
+    unsigned short up[kCapacity];
+    unsigned short down[kCapacity];
+    unsigned short row[kCapacity];
+    unsigned short col[kCapacity];
 
     FixedDlxNodeList(size_t capacity) {
     }
@@ -61,12 +62,12 @@ public:
 
 class DlxNodeList {
 public:
-    int * prev;
-    int * next;
-    int * up;
-    int * down;
-    int * row;
-    int * col;
+    unsigned short * prev;
+    unsigned short * next;
+    unsigned short * up;
+    unsigned short * down;
+    unsigned short * row;
+    unsigned short * col;
 
 private:
     size_t size_;
@@ -89,12 +90,12 @@ public:
 private:
     void init(size_t capacity) {
         assert(capacity > 0);
-        this->prev = new int[capacity];
-        this->next = new int[capacity];
-        this->up   = new int[capacity];
-        this->down = new int[capacity];
-        this->row  = new int[capacity];
-        this->col  = new int[capacity];
+        this->prev = new unsigned short[capacity];
+        this->next = new unsigned short[capacity];
+        this->up   = new unsigned short[capacity];
+        this->down = new unsigned short[capacity];
+        this->row  = new unsigned short[capacity];
+        this->col  = new unsigned short[capacity];
     }
 
     void destroy() {
@@ -151,35 +152,17 @@ private:
     std::vector<int>    answer_;
     int                 last_idx_;
 
-    int rows_[TotalSize + 1];
-    int cols_[TotalSize + 1];
-    int numbers_[TotalSize + 1];
-
     SmallBitMatrix2<9, 9>  bit_rows;        // [row][num]
     SmallBitMatrix2<9, 9>  bit_cols;        // [col][num]
     SmallBitMatrix2<9, 9>  bit_palaces;     // [palace][num]
 
+    unsigned short col_index_[Sudoku::TotalConditions + 1];
+
+    unsigned short rows_[TotalSize + 1];
+    unsigned short cols_[TotalSize + 1];
+    unsigned short numbers_[TotalSize + 1];
+
     std::vector<std::vector<int>> answers_;
-
-    struct StackInfo {
-        int index;
-        int row;
-
-        StackInfo() : index(0), row(0) {}
-        StackInfo(int index, int row) : index(index), row(row) {}
-
-        void set(int index, int row) {
-            this->index = index;
-            this->row = row;
-        }
-    };
-
-    enum StackState {
-        SearchNext,
-        BackTracking,
-        BackTrackingRetry,
-        Last
-    };
 
 public:
     DancingLinks(size_t nodes)
@@ -223,7 +206,7 @@ private:
             return first;
         }
         int min_col_index = first;
-        for (int i = list_.next[first]; i != 0 ; i = list_.next[i]) {
+        for (int i = list_.next[first]; i != 0; i = list_.next[i]) {
             int col_size = col_size_[i];
             if (col_size < min_col) {
                 assert(col_size >= 0);
@@ -245,7 +228,8 @@ private:
     }
 
     std::bitset<9> getUsable(size_t row, size_t col) {
-        size_t palace = row / 3 * 3 + col / 3;
+        // size_t palace = row / 3 * 3 + col / 3;
+        size_t palace = tables.roundTo3[row] + tables.div3[col];
         return ~(this->bit_rows[row] | this->bit_cols[col] | this->bit_palaces[palace]);
     }
 
@@ -254,28 +238,58 @@ private:
     }
 
     void fillNum(size_t row, size_t col, size_t num) {
-        size_t palace = row / 3 * 3 + col / 3;
+        // size_t palace = row / 3 * 3 + col / 3;
+        size_t palace = tables.roundTo3[row] + tables.div3[col];
         this->bit_rows[row].set(num);
         this->bit_cols[col].set(num);
         this->bit_palaces[palace].set(num);
     }
 
 public:
-    void init() {
-        int cols = this->cols();
+    int filter_unused_cols(char board[Sudoku::BoardSize]) {
+        std::memset(&this->col_index_[0], 0, sizeof(this->col_index_));
+
+        size_t pos = 0;
+        for (size_t row = 0; row < Rows; row++) {
+            for (size_t col = 0; col < Cols; col++) {
+                unsigned char val = board[pos];
+                if (val != '.') {
+                    size_t num = val - '1';
+                    this->col_index_[0      + pos           + 1] = 0xFFFF;
+                    this->col_index_[81 * 1 + row * 9 + num + 1] = 0xFFFF;
+                    this->col_index_[81 * 2 + col * 9 + num + 1] = 0xFFFF;
+                    size_t palace_x_9 = tables.palace_x_9[pos];
+                    this->col_index_[81 * 3 + palace_x_9 + num + 1] = 0xFFFF;
+                }
+                pos++;
+            }
+        }
+
+        size_t index = 1;
+        for (size_t i = 1; i < (Sudoku::TotalConditions + 1); i++) {
+            if (this->col_index_[i] == 0) {
+                this->col_index_[i] = (unsigned short)index;
+                index++;
+            }
+        }
+        return (int)(index - 1);
+    }
+
+    void init(char board[Sudoku::BoardSize]) {
+        int cols = this->filter_unused_cols(board);
         for (int col = 0; col <= cols; col++) {
             list_.prev[col] = col - 1;
             list_.next[col] = col + 1;
             list_.up[col] = col;
             list_.down[col] = col;
-            list_.row[col] = 0;
-            list_.col[col] = col;
+            //list_.row[col] = 0;
+            //list_.col[col] = col;
         }
         list_.prev[0] = cols;
         list_.next[cols] = 0;
 
         last_idx_ = cols + 1;
-        for (size_t i = 0; i < col_size_.size(); i++) {
+        for (size_t i = 0; i <= cols; i++) {
             col_size_[i] = 0;
         }
 
@@ -299,7 +313,7 @@ public:
         size_t pos = 0;
         for (size_t row = 0; row < Rows; row++) {
             for (size_t col = 0; col < Cols; col++) {
-                char val = board[pos++];
+                unsigned char val = board[pos++];
                 if (val == '.') {
                     empties++;
                 }
@@ -307,7 +321,7 @@ public:
                     size_t num = val - '1';
                     this->fillNum(row, col, num);
                 }
-            }   
+            }
         }
 
         // maxRows = filled * 1 + empties * 9;
@@ -321,23 +335,25 @@ public:
         pos = 0;
         for (size_t row = 0; row < Rows; row++) {
             for (size_t col = 0; col < Cols; col++) {
-                size_t palace = row / 3 * 3 + col / 3;
-                char val = board[pos++];
+                // size_t palace = row / 3 * 3 + col / 3;
+                unsigned char val = board[pos];
                 if (val == '.') {
+                    size_t palace = tables.palace[pos];
+                    size_t palace_x_9 = tables.palace_x_9[pos];
                     std::bitset<9> numsUsable = getUsable(row, col, palace);
-                    for (size_t number = 0; number <= (Numbers - 1); number++) {
+                    for (size_t number = 0; number < Numbers; number++) {
                         if (numsUsable.test(number)) {
                             int head = last_idx_;
                             int index = last_idx_;
 
-                            this->insert(index + 0, row_idx, (int)(0      + row * 9 + col + 1));
+                            this->insert(index + 0, row_idx, (int)(0      + pos              + 1));
                             this->insert(index + 1, row_idx, (int)(81 * 1 + row * 9 + number + 1));
                             this->insert(index + 2, row_idx, (int)(81 * 2 + col * 9 + number + 1));
-                            this->insert(index + 3, row_idx, (int)(81 * 3 + palace * 9 + number + 1));
+                            this->insert(index + 3, row_idx, (int)(81 * 3 + palace_x_9 + number + 1));
 
-                            this->rows_[row_idx] = (int)row;
-                            this->cols_[row_idx] = (int)col;
-                            this->numbers_[row_idx] = (int)number;
+                            this->rows_[row_idx] = (unsigned short)row;
+                            this->cols_[row_idx] = (unsigned short)col;
+                            this->numbers_[row_idx] = (unsigned short)number;
                             index += 4;
                             row_idx++;
 
@@ -348,32 +364,38 @@ public:
                     }
                 }
                 else {
+#if 0
                     size_t number = val - '1';
                     int head = last_idx_;
                     int index = last_idx_;
 
-                    this->insert(index + 0, row_idx, (int)(0      + row * 9 + col + 1));
+                    this->insert(index + 0, row_idx, (int)(0      + pos              + 1));
                     this->insert(index + 1, row_idx, (int)(81 * 1 + row * 9 + number + 1));
                     this->insert(index + 2, row_idx, (int)(81 * 2 + col * 9 + number + 1));
-                    this->insert(index + 3, row_idx, (int)(81 * 3 + palace * 9 + number + 1));
+                    size_t palace_x_9 = tables.palace_x_9[pos];
+                    this->insert(index + 3, row_idx, (int)(81 * 3 + palace_x_9 + number + 1));
 
-                    this->rows_[row_idx] = (int)row;
-                    this->cols_[row_idx] = (int)col;
-                    this->numbers_[row_idx] = (int)number;
+                    this->rows_[row_idx] = (unsigned char)row;
+                    this->cols_[row_idx] = (unsigned char)col;
+                    this->numbers_[row_idx] = (unsigned char)number;
                     index += 4;
                     row_idx++;
 
                     list_.next[index - 1] = head;
                     list_.prev[head] = index - 1;
                     last_idx_ = index;
+#endif
                 }                
-
+                pos++;
             }
         }
         assert(row_idx <= (maxRows + 1));
     }
 
     void insert(int index, int row, int col) {
+        int save_col = col;
+        col = this->col_index_[col];
+        assert(col != 0xFFFF);
         list_.prev[index] = index - 1;
         list_.next[index] = index + 1;
         list_.up[index] = list_.up[col];
@@ -412,10 +434,6 @@ public:
 
     void restore(int index) {
         assert(index > 0);
-        int next = list_.next[index];
-        int prev = list_.prev[index];
-        list_.prev[next] = index;
-        list_.next[prev] = index;
 
         for (int row = list_.up[index]; row != index; row = list_.up[row]) {
             for (int col = list_.prev[row]; col != row; col = list_.prev[col]) {
@@ -426,6 +444,11 @@ public:
                 col_size_[list_.col[col]]++;
             }
         }
+
+        int next = list_.next[index];
+        int prev = list_.prev[index];
+        list_.prev[next] = index;
+        list_.next[prev] = index;
     }
 
     bool search() {
@@ -434,7 +457,7 @@ public:
                 this->answers_.push_back(this->answer_);
                 if (kSearchMode == SearchMode::MoreThanOneAnswer) {
                     if (this->answers_.size() > 1)
-                    return true;
+                        return true;
                 }
             }
             else {
@@ -481,8 +504,9 @@ public:
     }
 
     bool solve() {
+#if 0
 RETRY_LOCKED_CANDIDATES:
-        for (int index = list_.next[0]; index != 0 ; index = list_.next[index]) {
+        for (int index = list_.next[0]; index != 0; index = list_.next[index]) {
             size_t col_size = col_size_[index];
             if (col_size == 1) {
                 assert(index > 0);
@@ -502,7 +526,7 @@ RETRY_LOCKED_CANDIDATES:
                 return false;
             }
         }
-
+#endif
         return this->search();
     }
 
@@ -518,7 +542,6 @@ RETRY_LOCKED_CANDIDATES:
 
     void display_answers(char board[Sudoku::BoardSize]) {
         printf("Total answers: %d\n\n", (int)this->answers_.size());
-#if 0
         int i = 0;
         for (auto answer : this->answers_) {
             Sudoku::clear_board(board);
@@ -529,8 +552,9 @@ RETRY_LOCKED_CANDIDATES:
             }
             Sudoku::display_board(board, false, i);
             i++;
+            if (i > 100)
+                break;
         }
-#endif
     }
 };
 
@@ -562,7 +586,7 @@ public:
         jtest::StopWatch sw;
         sw.start();
 
-        solver_.init();
+        solver_.init(board);
         solver_.build(board);
         bool success = solver_.solve();
 
@@ -574,7 +598,7 @@ public:
                 solver_.display_answers(board);
             else
                 solver_.display_answer(board);
-            printf("Elapsed time: %0.3f ms, init_counter: %" PRIuPTR ", recur_counter: %" PRIuPTR "\n\n"
+            printf("elapsed time: %0.3f ms, init_counter: %" PRIuPTR ", recur_counter: %" PRIuPTR "\n\n"
                    "num_guesses: %" PRIuPTR ", num_impossibles: %" PRIuPTR ", no_guess: %" PRIuPTR "\n"
                    "guess %% = %0.1f %%, impossible %% = %0.1f %%, no_guess %% = %0.1f %%\n\n",
                    elapsed_time, DancingLinks::get_init_counter(),
